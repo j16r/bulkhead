175f6d8ee78e851c229f810bfd8262fd44b0b222
John Barker <jebarker@gmail.com>
2014-10-11 17:50:43 -0400

    First test passes: POST /sessions responds with text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate iron;
+extern crate router;
+
+use std::io::net::ip::Ipv4Addr;
+
+use iron::{Iron, Request, Response, IronResult};
+use router::{Router, Params};
+use iron::status;
+
+fn new_session_handler(_: &mut Request) -> IronResult<Response> {
+  Ok(Response::with(status::Ok, "AUTHZ!"))
+}
+
 fn main() {
-    println!("Hello, world!")
+  let mut router = Router::new();
+  router.post("/sessions", new_session_handler);
+
+  Iron::new(router).listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
