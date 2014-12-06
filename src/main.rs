b2057155ff5854434c394f37fc5a42263f832963
John Barker <jebarker@gmail.com>
2014-12-05 21:56:07 -0500

    Upgrade upstream packages, use crates.io registry for all packages

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ extern crate serialize;
 extern crate postgres;
 extern crate time;
 
-use std::io::net::ip::Ipv4Addr;
 use serialize::json;
-
-use iron::{Iron, Request, Response, IronResult};
-use router::{Router, Params};
+use iron::{Iron, IronResult, Request, Response};
+use iron::modifier::Set;
+use iron::response::modifiers::{Status, Body};
 use iron::status;
-use postgres::{PostgresConnection, NoSsl};
-use postgres::types::ToSql;
+use router::Router;
+use postgres::{Connection, SslMode};
 use time::Timespec;
 
 #[deriving(Decodable, Encodable)]
@@ -24,17 +23,17 @@ pub struct SessionResponse {
   session: Session
 }
 
-fn db_connect() -> PostgresConnection {
-  PostgresConnection::connect("postgres://bulkhead@localhost",
-                              &NoSsl).unwrap()
+fn db_connect() -> Connection {
+  Connection::connect("postgres://bulkhead@localhost",
+                      &SslMode::None).unwrap()
 }
 
-fn migrate(conn: &PostgresConnection) {
+fn migrate(conn: &Connection) {
   conn.execute("CREATE TABLE IF NOT EXISTS users (
                   id              SERIAL PRIMARY KEY,
                   name            VARCHAR NOT NULL,
                   created_at      TIMESTAMP NOT NULL
-                )", []).unwrap();
+                )", &[]).unwrap();
   conn.execute("CREATE TABLE IF NOT EXISTS sessions (
                   id              SERIAL PRIMARY KEY,
                   user_id         int,
@@ -42,7 +41,7 @@ fn migrate(conn: &PostgresConnection) {
 
                   CONSTRAINT fk_user FOREIGN KEY (user_id)
                     REFERENCES users (id)
-                )", []).unwrap();
+                )", &[]).unwrap();
 }
 
 struct User {
@@ -75,14 +74,14 @@ fn authenticate_user(new_session_request : &NewSessionRequest) -> Option<User> {
 
 fn create_session(user: &User) -> Option<Session> {
   let db = db_connect();
-  let stmt = db.execute("INSERT INTO sessions (user_id, created_at)
-                         VALUES($1, now())",
-                        &[&user.id]).unwrap();
+  db.execute("INSERT INTO sessions (user_id, created_at)
+              VALUES($1, now())",
+             &[&user.id]).ok();
   let stmt = db.prepare("SELECT
                           id
                          FROM sessions
                          WHERE id = lastval()").unwrap();
-  for row in stmt.query([]).unwrap() {
+  for row in stmt.query(&[]).unwrap() {
     return Some(Session {
       id: row.get(0u),
     })
@@ -91,15 +90,20 @@ fn create_session(user: &User) -> Option<Session> {
 }
 
 fn new_session_handler(req: &mut Request) -> IronResult<Response> {
-  let new_session_request : NewSessionRequest = json::decode(req.body.as_slice()).unwrap();
+  let new_session_request : NewSessionRequest = json::decode(req.body.to_string().as_slice()).unwrap();
 
   let user = match authenticate_user(&new_session_request) {
     Some(user) => user,
-    None => return Ok(Response::with(status::Unauthorized, "Unauthorized!"))
+    None => return Ok(
+        Response::new()
+            .set(Status(status::Unauthorized))
+            .set(Body("Unauthorized!")))
   };
 
   let response = SessionResponse{session: create_session(&user).unwrap()};
-  Ok(Response::with(status::Ok, json::encode(&response)))
+  Ok(Response::new()
+     .set(Status(status::Ok))
+     .set(Body(json::encode(&response))))
 }
 
 fn main() {
@@ -109,7 +113,9 @@ fn main() {
   let mut router = Router::new();
   router.post("/sessions", new_session_handler);
 
-  let mut server = Iron::new(router);
-  server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
+  Iron::new(router)
+      .listen("0.0.0.0:3000")
+      .unwrap_or_else(|error| panic!("Failed to listen {}", error));
+
   println!("Bulkhead ready.");
 }
--- tests/new_session.rs
+++ tests/new_session.rs
@@ -1,49 +1,50 @@
 extern crate hyper;
 
+use std::io::Process;
 use std::io::Command;
 use std::io::timer::Timer;
 use std::time::duration::Duration;
-use std::io::MemWriter;
-use std::io::util::copy;
 
 use hyper::Url;
 use hyper::client::Request;
 
-fn setup() {
-  spawn(proc() {
-    match Command::new("target/bulkhead").spawn() {
-      Ok(_) => println!("bulkhead server running..."),
-      Err(msg) => fail!("Failed to launch bulkhead server: {}", msg)
-    }
-  });
+struct Context {
+  server: Process
+}
+
+fn setup() -> Context {
+  let server = Command::new("target/bulkhead")
+    .spawn()
+    .unwrap_or_else(|msg| panic!("Failed to launch bulkhead server: {}", msg));
 
   Timer::new().unwrap().sleep(Duration::seconds(1));
+  println!("bulkhead server running...");
+
+  Context {server: server}
+}
+
+fn teardown(context: &mut Context) {
+  //context.server.kill();
 }
 
 #[test]
 fn new_session_test() {
-  setup();
+  let mut ctx = setup();
 
   let new_session_url = Url::parse("http://localtest.me:3000/sessions").unwrap();
-  let request = match Request::post(new_session_url) {
-    Ok(request) => request,
-    Err(err) => fail!("Failed to connect to bulkhead on {}", err)
-  };
+  let request = Request::post(new_session_url)
+    .unwrap_or_else(|error| panic!("Failed to connect to bulkhead on {}", error));
 
-  let mut stream = match request.start() {
-    Ok(stream) => stream,
-    Err(err) => fail!("Failed to write to request {}", err)
-  };
+  let mut stream = request.start()
+    .unwrap_or_else(|error| panic!("Failed to write to request {}", error));
 
-  stream.write("{\"session\":{\"username\": \"timmy\", \"password\":\"1234\"}}".as_bytes());
-  let mut response = match stream.send() {
-    Ok(response) => response,
-    Err(err) => fail!("Failed to read response {}", err)
-  };
+  stream.write("{\"session\":{\"username\": \"timmy\", \"password\":\"1234\"}}".as_bytes())
+    .unwrap();
 
-  let mut body = MemWriter::new();
-  copy(&mut response, &mut body);
-  let output = String::from_utf8(body.unwrap()).unwrap();
+  let mut response = stream.send()
+    .unwrap_or_else(|error| panic!("Failed to read response {}", error));
 
-  assert_eq!(output.as_slice(), "{\"session\":{\"id\":1}}");
+  assert_eq!(response.read_to_string().unwrap().as_slice(), "{\"session\":{\"id\":1}}");
+
+  //teardown(&ctx);
 }
