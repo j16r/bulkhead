453f401fbb26bc9b9a6f7b13300a46b353ac74dd
John Barker <jebarker@gmail.com>
2015-07-21 22:29:25 -0400

    Update upstream rust, use new clone() api in r2d2

--- src/db.rs
+++ src/db.rs
@@ -2,30 +2,31 @@ use iron::BeforeMiddleware;
 use iron::prelude::*;
 use iron::typemap::Key;
 use postgres::SslMode;
-use r2d2::{Pool, NoopErrorHandler, PooledConnection};
+use r2d2::{Config, Pool, LoggingErrorHandler, PooledConnection};
 use r2d2_postgres::{PostgresConnectionManager};
 use std::default::Default;
 use std::sync::Arc;
 
 pub struct DbPool {
-    pool: Arc<Pool<PostgresConnectionManager>>
+    pool: Pool<PostgresConnectionManager>
 }
 
 impl DbPool {
     pub fn new() -> DbPool {
-        let config = Default::default();
+        let config = Config::builder()
+            .error_handler(Box::new(LoggingErrorHandler))
+            .build();
         let manager = PostgresConnectionManager::new("postgres://bulkhead@localhost",
                                                      SslMode::None)
             .unwrap();
-        let error_handler = Box::new(NoopErrorHandler);
-        let pool = Arc::new(
-            Pool::new(config, manager, error_handler).unwrap());
+        let pool = Pool::new(config, manager).unwrap();
 
         DbPool {pool: pool}
     }
 
     fn migrate(&self) {
-        let connection = Pool::get_arc(self.pool.clone()).unwrap();
+        let pool = self.pool.clone();
+        let connection = pool.get().unwrap();
         connection.execute("CREATE TABLE IF NOT EXISTS users (
                                 id         SERIAL PRIMARY KEY,
                                 name       VARCHAR NOT NULL,
@@ -42,7 +43,7 @@ impl DbPool {
     }
 }
 
-pub type DbConnection = PooledConnection<'static, PostgresConnectionManager>;
+pub type DbConnection = PooledConnection<PostgresConnectionManager>;
 
 impl Key for DbPool {
     type Value = DbConnection;
@@ -50,7 +51,8 @@ impl Key for DbPool {
 
 impl BeforeMiddleware for DbPool {
     fn before(&self, request: &mut Request) -> IronResult<()> {
-        request.extensions.insert::<DbPool>(Pool::get_arc(self.pool.clone()).unwrap());
+        let pool = self.pool.clone();
+        request.extensions.insert::<DbPool>(pool.get().unwrap());
         Ok(())
     }
 }
